8df530026240a880571f1ff823a4e8490042c610
CodeWizard-bip <atlasnexus193@gmail.com>
2023-11-28 08:09:18 -0300

    para desplegar

---
+++ README.md
@@ -0,0 +1,108 @@
+## PI_1-MLOps Juegos Steam - Modelo de recomendación
+
+Proyecto Individual 1 de Machine Learning en bootcamp Henry
+
+### Descripción del proyecto:
+
+El proyecto tiene como objetivo simular la función de un MLOps Engineer, fusionando las responsabilidades de un Data Engineer y un Data Scientist, dentro del entorno de la plataforma de juegos Steam. Abordamos un problema de negocio específico mediante la creación de un Producto Mínimo Viable (MVP), que consta de una API desplegada y un modelo de Machine Learning. Este modelo realiza un análisis de sentimientos basado en los comentarios de los usuarios y proporciona un sistema de recomendación de videojuegos para mejorar la experiencia en la plataforma.
+
+### Archivos para prepar:
+
+Archivos de tipo JSON GZIP:
+
++ **output_steam_games.json** contiene información sobre los juegos; como nombre de los juegos, editores, dessarrolladores.
+
++ **australian_users_items.json** contiene información de los juegos que utilizan los usuarios, y el tiempo consumido de cada usuario.
+
++ **autralian_users_reviews.json** contiene los comentarios de los usuarios sobre los juegos que utilizan , recomendaciones, datos url y user_id.
+
+Diccionario de datos [Dataset](/) 
+
+
+### Rol Data Engineer:
+
+#### ETL Extracción, Transformación y Carga
+
+Notebooks [ETL](/ETL.ipynb).
+
+En el archivo ETL se lleva a cabo la transformación de los conjuntos de datos, preparando las columnas y filas. Se verifica y ajusta los tipos de datos, y se imputan o eliminan valores faltantes, todo ello con el propósito de preparar los datos para la carga.
+
+
+#### Análisis Exploratorio de los Datos
+
+Notebook [Analisis exploratorio](/EDA.ipynb)
+
+En el archivo EDA se formulan preguntas de negocio con el objetivo de optimizar los servicios ofrecidos por la plataforma. Se analizan los datos y se buscan correlaciones para lograr la máxima eficiencia, reduciendo costos y maximizando beneficios.
+
+### Desarrollo de Funciones para la API
+
+FastAPI sera la herramienta para desplegar:
+
+def PlayTimeGenre( genero : str ): Debe devolver año con mas horas jugadas para dicho género.
+Ejemplo de retorno: {"Año de lanzamiento con más horas jugadas para Género X" : 2013}
+
+def UserForGenre( genero : str ): Debe devolver el usuario que acumula más horas jugadas para el género dado y una lista de la acumulación de horas jugadas por año.
+Ejemplo de retorno: {"Usuario con más horas jugadas para Género X" : us213ndjss09sdf, "Horas jugadas":[{Año: 2013, Horas: 203}, {Año: 2012, Horas: 100}, {Año: 2011, Horas: 23}]}
+
+def UsersRecommend( año : int ): Devuelve el top 3 de juegos MÁS recomendados por usuarios para el año dado. (reviews.recommend = True y comentarios positivos/neutrales)
+Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+
+def UsersWorstDeveloper( año : int ): Devuelve el top 3 de desarrolladoras con juegos MENOS recomendados por usuarios para el año dado. (reviews.recommend = False y comentarios negativos)
+Ejemplo de retorno: [{"Puesto 1" : X}, {"Puesto 2" : Y},{"Puesto 3" : Z}]
+
+def sentiment_analysis( empresa desarrolladora : str ): Según la empresa desarrolladora, se devuelve un diccionario con el nombre de la desarrolladora como llave y una lista con la cantidad total de registros de reseñas de usuarios que se encuentren categorizados con un análisis de sentimiento como valor.
+Ejemplo de retorno: {'Valve' : [Negative = 182, Neutral = 120, Positive = 278]}
+
+sistema de recomendación user-item:
+
+def recomendacion_usuario( id de usuario ): Ingresando el id de un usuario, deberíamos recibir una lista con 5 juegos recomendados para dicho usuario.
+
+
+### Modelamiento (Machine Learning Model Development)
+
+Notebook [Modelo_Recomendacion](/Modelo_Recomendacion.ipynb)
+
+Se crea la matriz de correlacion que representa las correlaciones de los productos mediante la funcion "similitud de coseno" para preparar la funcion "def recomendacion_usuario( id de usuario )".
+
+### FastAPI
+
+Archivo [Main](/main.py). Para ejecutar la API desde localHost seguir los pasos que se detallan acontinuacion:
+
+- Clonar el proyecto haciendo `git clone`.
+- Crear el entorno de trabajo en Visual Studio Code:
+    * Crear entorno `python -m venv env`
+    * Ingresar al entorno haciendo `env\Scripts\activate`
+    * Instalar dependencias con `pip install -r requirements.txt`
+- Ejecutar el archivo `main.py` desde consola activando uvicorn. Para ello, hacer `uvicorn main:app --reload`
+- Hacer Ctrl + clic sobre la dirección `http://XXX.X.X.X:XXXX` (se muestra en la consola).
+- Una vez en el navegador, agregar `/docs` para acceder a ReDoc.
+- En cada una de las funciones hacer clic en *Try it out* y luego introducir el dato que requiera o utilizar los ejemplos por defecto. Finalmente Ejecutar y observar la respuesta.
+
+### Deploy 
+
+deploy de la API plataforma Render plataforma para crear y ejecutar aplicaciones y sitios web. 
+
+* Se crea un servicio en `render.com`, conectando al repositorio
+
+* Enlace de la aplicacion https://pi1-steamgames-deploy-jimefioni.onrender.com 
+
+### Video
+
+demostración del funcionamiento de la API [Video](http)
+
+![Pandas](https://img.shields.io/badge/-Pandas-333333?style=flat&logo=pandas)
+![Numpy](https://img.shields.io/badge/-Numpy-333333?style=flat&logo=numpy)
+![Matplotlib](https://img.shields.io/badge/-Matplotlib-333333?style=flat&logo=matplotlib)
+![Seaborn](https://img.shields.io/badge/-Seaborn-333333?style=flat&logo=seaborn)
+![Scikitlearn](https://img.shields.io/badge/-Scikitlearn-333333?style=flat&logo=scikitlearn)
+![FastAPI](https://img.shields.io/badge/-FastAPI-333333?style=flat&logo=fastapi)
+![TextBlob](https://img.shields.io/badge/-TextBlob-333333?style=flat&logo=textblob)
+![Render](https://img.shields.io/badge/-Render-333333?style=flat&logo=render)
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,244 @@
+from fastapi import FastAPI, HTTPException
+import pandas as pd
+import fastparquet
+import dask.dataframe as dd
+
+
+
+
+
+
+app = FastAPI()
+
+ruta_archivo = 'Data/genero_play_time.parquet'  
+horas_usuario = pd.read_parquet(ruta_archivo)
+
+@app.get("/PlayTimeGenre/{genero}")
+async def PlayTimeGenre(genero: str):
+    """
+    Obtén el año con más horas jugadas para un género específico.
+
+    Parámetros:
+    - genero (str): Género para el cual se desea obtener el año con más horas jugadas.
+
+    Ejemplo de uso:
+    - /PlayTimeGenre/Action
+
+    Retorna:
+    - dict: {"Año con más horas jugadas para el género": genero, "Año": max_playtime_year}
+    """
+    df_genero = horas_usuario[horas_usuario['genres'].str.contains(genero, na=False)]
+  
+    df_grouped = df_genero.groupby('Año_estreno')['playtime_forever'].sum()
+    
+    max_playtime_year = df_grouped.idxmax()
+    
+    return {"Año con más horas jugadas para el género": genero, "Año": max_playtime_year}
+
+
+
+
+
+df_horas_usuario = pd.read_parquet('Data/user_for_genero_mitad.parquet')
+
+@app.get("/UserForGenre/{genero}")
+async def UserForGenre(genero: str):
+    """
+    Obtén información sobre el usuario con más horas jugadas y la acumulación de horas jugadas por año para un género específico.
+
+    Parámetros:
+    - genero (str): Género para el cual se desea obtener la información.
+
+    Ejemplo de uso:
+    - /UserForGenre/Action
+
+    Retorna:
+    - dict: {
+        "Usuario con más horas jugadas para Género": usuario_con_mas_horas,
+        "Horas jugadas": lista_acumulacion
+      }
+
+    Si no hay datos para el género, el mensaje será:
+    - {"mensaje": "No hay datos para el género '{genero}'."}
+    """
+    df_genre = df_horas_usuario[df_horas_usuario['genres'] == genero]
+
+    if df_genre.empty:
+        return {"mensaje": f"No hay datos para el género '{genero}'."}
+
+    usuario_con_mas_horas = df_genre.groupby('user_id')['playtime_forever'].sum().idxmax()
+
+    acumulacion_por_anio = df_genre.groupby('Año_estreno')['playtime_forever'].sum().reset_index()
+    acumulacion_por_anio = acumulacion_por_anio.rename(columns={"playtime_forever": "Horas"})
+    lista_acumulacion = acumulacion_por_anio.to_dict(orient='records')
+
+    salida = {
+        "Usuario con más horas jugadas para Género": usuario_con_mas_horas,
+        "Horas jugadas": lista_acumulacion
+    }
+
+    return salida
+
+
+recomendado = pd.read_parquet('Data/Recommend_Users2.parquet')
+
+@app.get('/UsersRecommend/{anio}')
+async def UsersRecommend(anio: int):
+    """
+    Obtiene las principales recomendaciones de juegos para un año específico.
+
+    Parámetros:
+    - anio (int): Año para el cual se desean obtener las recomendaciones de juegos.
+
+    Ejemplo de uso:
+    - /UsersRecommend/2022
+
+    Retorna:
+    - list: [
+        {"Puesto 1": nombre_juego_1},
+        {"Puesto 2": nombre_juego_2},
+        {"Puesto 3": nombre_juego_3}
+      ]
+
+    Si no hay datos para el año, se lanza una excepción HTTP con código 404.
+    Si no hay juegos recomendados para el año, se lanza una excepción HTTP con código 404.
+    Si se produce un error inesperado, se lanza una excepción HTTP con código 500.
+    """
+    try:
+        # Filtrar por año
+        df_year = recomendado[recomendado['Año_estreno'] == anio]
+
+        # Verificar si hay datos para el año
+        if df_year.empty:
+            raise HTTPException(status_code=404, detail=f"No hay datos para el año {anio}.")
+
+        # Filtrar recomendaciones positivas
+        df_recommendations = df_year[(df_year['recommend'] == True) & (df_year['sentiment_analisis'] >= 1)]
+
+        # Verificar si hay juegos recomendados
+        if df_recommendations.empty:
+            raise HTTPException(status_code=404, detail=f"No hay juegos recomendados para el año {anio}.")
+
+        # Agrupar por juego y sumar sentimientos
+        grouped_games = df_recommendations.groupby('item_name')['sentiment_analisis'].sum()
+
+        # Obtener los tres juegos principales
+        top_3_games = grouped_games.nlargest(3)
+
+        # Formatear la salida como una lista de diccionarios
+        output_list = [{"Puesto 1": top_3_games.index[0]},
+                       {"Puesto 2": top_3_games.index[1]},
+                       {"Puesto 3": top_3_games.index[2]}]
+
+        return output_list
+
+    except Exception as e:
+        # Manejar errores inesperados con una excepción HTTP 500
+        raise HTTPException(status_code=500, detail=f"Se produjo un error inesperado: {str(e)}")
+
+    
+df_recomendado = pd.read_parquet('Data/Recommend_Users2.parquet')
+
+@app.get('/UsersWorstDeveloper/{anio}')
+async def UsersWorstDeveloper(anio: int):
+    """
+    Obtiene los juegos menos recomendados por los desarrolladores para un año específico.
+
+    Parámetros:
+    - anio (int): Año para el cual se desean obtener los juegos menos recomendados.
+
+    Ejemplo de uso:
+    - /UsersWorstDeveloper/2022
+
+    Retorna:
+    - list: [
+        {"Puesto 1": nombre_juego_1},
+        {"Puesto 2": nombre_juego_2},
+        {"Puesto 3": nombre_juego_3}
+      ]
+
+    Si no hay datos para el año o no hay juegos menos recomendados, se lanza una excepción HTTP con código 404.
+    Si se produce un error inesperado, se lanza una excepción HTTP con código 500.
+    """
+    try:
+        # Filtrar por año
+        df_year = df_recomendado[df_recomendado['Año_estreno'] == anio]
+
+        # Filtrar juegos menos recomendados
+        df_not_recommendations = df_year[(df_year['recommend'] == False) & (df_year['sentiment_analisis'] == 0)]
+
+        # Verificar si hay datos de juegos menos recomendados
+        if df_not_recommendations.empty:
+            raise HTTPException(status_code=404, detail=f"No hay datos de juegos menos recomendados para el año {anio} con los filtros especificados.")
+
+        # Contar juegos menos recomendados
+        juegos_menos_recomendados = df_not_recommendations['item_name'].value_counts().reset_index()
+        juegos_menos_recomendados.columns = ['item_name', 'count']
+
+        # Obtener los tres juegos menos recomendados
+        top_juegos_menos_recomendados = juegos_menos_recomendados.nlargest(3, 'count')
+
+        # Crear la lista de salida en el formato deseado
+        resultado = [{"Puesto 1": top_juegos_menos_recomendados.iloc[0]['item_name']},
+                     {"Puesto 2": top_juegos_menos_recomendados.iloc[1]['item_name']},
+                     {"Puesto 3": top_juegos_menos_recomendados.iloc[2]['item_name']}]
+
+        return resultado
+
+    except Exception as e:
+        # Manejar errores inesperados con una excepción HTTP 500
+        raise HTTPException(status_code=500, detail=f"Se produjo un error inesperado: {str(e)}")
+
+
+analisis_sentimiento = pd.read_parquet('Data/Sentiment.parquet')
+
+@app.get('/sentiment_analysis/{desarrolladora}')
+def sentiment_analysis(desarrolladora: str):
+    """
+    Realiza análisis de sentimiento para una desarrolladora específica.
+
+    Parámetros:
+    - desarrolladora (str): Nombre de la desarrolladora para la cual se realiza el análisis de sentimiento.
+
+    Ejemplo de uso:
+    - /sentiment_analysis/EjemploDevelopers
+
+    Retorna:
+    - dict: {
+        "desarrolladora": [
+            "Negative = cantidad",
+            "Neutral = cantidad",
+            "Positive = cantidad"
+        ]
+      }
+
+    Si no hay datos para la desarrolladora, se devuelve un mensaje indicando la ausencia de datos.
+    """
+    try:
+        # Mapeo de valores de sentimiento
+        sentiment_mapping = {0: 'Negative', 1: 'Neutral', 2: 'Positive'}
+
+        # Filtrar por desarrolladora
+        df_developer = analisis_sentimiento[analisis_sentimiento['developer'] == desarrolladora]
+
+        # Verificar si hay datos para la desarrolladora
+        if df_developer.empty:
+            return {desarrolladora: "No hay datos para la desarrolladora."}
+
+        # Mapear los valores de sentimiento
+        df_developer['sentiment_analisis'] = df_developer['sentiment_analisis'].map(sentiment_mapping)
+
+        # Contar la frecuencia de cada sentimiento
+        sentiment_counts = df_developer['sentiment_analisis'].value_counts().to_dict()
+
+        # Crear la lista de salida en el formato deseado
+        output_list = [f"{key} = {value}" for key, value in sentiment_counts.items()]
+
+        return {desarrolladora: output_list}
+
+    except Exception as e:
+        # Manejar errores inesperados con una respuesta genérica
+        return {"message": f"Se produjo un error inesperado: {str(e)}"}
+
+
+
